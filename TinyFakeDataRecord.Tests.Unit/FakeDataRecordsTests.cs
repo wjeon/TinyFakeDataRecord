16aad064c8b8647a9759e30f43c7ba3181255bed
wjeon <novajeon@yahoo.com>
2015-11-25 02:28:45 -0800

    TFDR-001 Added MetaData parameter in FakeDataRecords constructor

--- TinyFakeDataRecord.Tests.Unit/FakeDataRecordsTests.cs
+++ TinyFakeDataRecord.Tests.Unit/FakeDataRecordsTests.cs
@@ -7,9 +7,10 @@ namespace TinyFakeDataRecord.Tests.Unit
     public class FakeDataRecordsTests
     {
         [Test]
-        public void When_create_FakeDataRecords_it_initializes_fake_data_records_with_object_arry_list()
+        public void When_create_FakeDataRecords_it_constructs_with_meta_data_and_initializes_fake_data_records_with_object_arry_list()
         {
-            var fakeDataRecords = new FakeDataRecords();
+            var metaData = new MetaData(new Field[] {});
+            var fakeDataRecords = new FakeDataRecords(metaData);
             var records = fakeDataRecords.ToList();
             Assert.That(records.GetType(), Is.EqualTo(typeof(List<object[]>)));
         }
--- TinyFakeDataRecord/FakeDataRecords.cs
+++ TinyFakeDataRecord/FakeDataRecords.cs
@@ -5,9 +5,11 @@ namespace TinyFakeDataRecord
     public class FakeDataRecords
     {
         private readonly List<object[]> _records;
+        private readonly MetaData _metaData;
 
-        public FakeDataRecords()
+        public FakeDataRecords(MetaData metaData)
         {
+            _metaData = metaData;
             _records = new List<object[]>();
         }
 
