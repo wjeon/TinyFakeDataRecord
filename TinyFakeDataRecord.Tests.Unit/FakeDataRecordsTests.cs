04aa23245abb86bfccb003287b360a9b42f84697
wjeon <novajeon@yahoo.com>
2015-11-25 01:29:05 -0800

    TFDR-001 Added FakeDataRecords class

---
+++ TinyFakeDataRecord.Tests.Unit/FakeDataRecordsTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TinyFakeDataRecord.Tests.Unit
+{
+    [TestFixture]
+    public class FakeDataRecordsTests
+    {
+        [Test]
+        public void When_create_FakeDataRecords_it_initializes_fake_data_records_with_object_arry_list()
+        {
+            var fakeDataRecords = new FakeDataRecords();
+            var records = fakeDataRecords.ToList();
+            Assert.That(records.GetType(), Is.EqualTo(typeof(List<object[]>)));
+        }
+    }
+}
---
+++ TinyFakeDataRecord/FakeDataRecords.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TinyFakeDataRecord
+{
+    public class FakeDataRecords
+    {
+        private readonly List<object[]> _records;
+
+        public FakeDataRecords()
+        {
+            _records = new List<object[]>();
+        }
+
+        public List<object[]> ToList()
+        {
+            return _records;
+        }
+    }
+}
