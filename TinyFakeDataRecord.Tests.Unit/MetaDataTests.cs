47c367b2d53df59c2b4208115130dd2a5c63a061
wjeon <novajeon@yahoo.com>
2015-11-25 02:19:18 -0800

    TFDR-001 Added MetaData class

---
+++ TinyFakeDataRecord.Tests.Unit/MetaDataTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace TinyFakeDataRecord.Tests.Unit
+{
+    [TestFixture]
+    public class MetaDataTests
+    {
+        [Test]
+        public void Construct_MetaData_with_Field_array()
+        {
+            var fields = new []
+                {
+                    new Field("Test_Field_1", DataType.adInteger),
+                    new Field("Test_Field_2", DataType.adVarChar)
+                };
+            var metaData = new MetaData(fields);
+
+            Assert.That(metaData.Fields, Is.EqualTo(fields));
+        }
+    }
+}
